978f7832bc01368ea7874fd24fcd2f1f997ec2a6
Fredy Piraquive <40009527+Judiu@users.noreply.github.com>
2024-01-25 15:56:23 -0500

    Add Three.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
+import three from "three";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), three()],
   i18n:{
     defaultLocale: 'es',
     locales: ['es','en','pt'],
