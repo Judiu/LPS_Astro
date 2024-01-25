7e8f71c13eac9276022ae741e498dbef2f623b00
Fredy Piraquive <40009527+Judiu@users.noreply.github.com>
2024-01-24 22:10:48 -0500

    Se agrega Portuguese

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,11 +1,15 @@
 import english from './en.json';
 import spanish from './es.json';
+import portuguese from './pt.json'
 
 const languages = {
     english:'en',
+    portuguese: 'pt',
     spanish:'es'};
 
 export const getI18N = ({ currentLocale = 'es'} : {currentLocale : string|undefined}) => {
     if (currentLocale == languages.english) return english
+    if (currentLocale == languages.portuguese) return portuguese
     if (currentLocale == languages.spanish) return spanish
+    
     }
