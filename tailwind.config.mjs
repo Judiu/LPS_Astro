00775584865975213af94bbfecb988a52b05cc2a
Fredy Piraquive <40009527+Judiu@users.noreply.github.com>
2024-01-29 21:24:22 -0500

    Add Ferroscan.astro
    Page for Ferroscan.astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',"./node_modules/tw-elements/dist/js/**/*.js"],
 	theme: {
 		extend: {
+			fontFamily: {
+				'mono': ['Courier New', 'monospace'],
+			},
 			colors: {
 				'LPSColor' : '#F4041B'
 			}
