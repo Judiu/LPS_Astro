653bf70b31b17f3e0215650fa721a54791b97006
Fredy Piraquive <40009527+Judiu@users.noreply.github.com>
2024-01-30 23:26:13 -0500

    update Links

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -114,7 +114,7 @@ export default function App() {
                   {i18n?.BENTO_1_TITLE}
                 </div>
                 <div className="subtitle text-xl sm:text-4xl text-LPSColor" data-swiper-parallax="-200">
-                  Para Informes de Curaduria
+                  Para Informes de Estructurales
                 </div>
                 <div className="text text-sm sm:text-xl text-LPSColor " data-swiper-parallax="-100">
                   <p slot="content" className="">
